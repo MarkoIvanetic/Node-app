750e7349235b712576599842c81fdd8e6a97effa
MarkoIvanetic <manna.grad@gmail.com>
2016-01-12 12:36:46 +0100

    Handlebars implementation

--- router/router.js
+++ router/router.js
@@ -1,9 +1,20 @@
 module.exports = function(app)
 {
-     app.get('/',function(req,res){
-        res.render('index.html')
-     });
-     app.get('/about',function(req,res){
-        res.render('about.html');
-    });
+	app.use(function(req, res, next){
+		res.locals.showTests = app.get('env') !== 'production' &&
+		req.query.test === '1';
+		next();
+	});
+	app.get('/',function(req,res){
+		res.render('index')
+	});
+	app.get('/about',function(req,res){
+		res.render('about');
+	});     
+	app.get('/shop',function(req,res){
+		res.render('shop');
+	});     
+	app.get('/*',function(req,res){
+		res.send(req.url);
+	});
 }
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express    =    require('express');
+var exphbs  = require('express-handlebars');	
 var app        =    express();
 
+app.engine('handlebars', exphbs({defaultLayout: 'about'}));
+app.set('view engine', 'handlebars');
+
 require('./router/router')(app);
-app.set('views',__dirname + '/views');
-console.log(__dirname);
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.set('views',__dirname + '/views/layouts');
+
 
 var server = app.listen(3000,function(){
 console.log("Express is running on port 3000");
